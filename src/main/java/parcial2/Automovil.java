85256ac57167b5260dbaeed7d592bce3d2c594a4
invitado <invitado@gmail.com>
2024-06-06 08:15:56 -0500

    Automovil

--- src/main/java/parcial2/Automovil.java
+++ src/main/java/parcial2/Automovil.java
@@ -1,5 +1,33 @@
 package parcial2;
 
-public class Automovil {
-    // TODO: Tu código va aquí
+public class Automovil extends Vehiculo {
+    private int numeroPuertas;
+    private TipoCombustible tipoCombustible;
+
+    public Automovil(String marca, String modelo, int año, int numeroPuertas, TipoCombustible tipoCombustible) {
+        super(marca, modelo, año);
+        this.numeroPuertas = numeroPuertas;
+        this.tipoCombustible = tipoCombustible;
+    }
+
+    @Override
+    public String imprimirInformacion() {
+        return super.imprimirInformacion() + ", " + numeroPuertas + " puertas, combustión a " + tipoCombustible;
+    }
+
+    public int getNumeroPuertas() {
+        return numeroPuertas;
+    }
+
+    public void setNumeroPuertas(int numeroPuertas) {
+        this.numeroPuertas = numeroPuertas;
+    }
+
+    public TipoCombustible getTipoCombustible() {
+        return tipoCombustible;
+    }
+
+    public void setTipoCombustible(TipoCombustible tipoCombustible) {
+        this.tipoCombustible = tipoCombustible;
+    }
 }
