8aa3f8bddfd99b0c972fdbfb52a52dd9744c7afc
invitado <invitado@gmail.com>
2024-06-06 08:40:06 -0500

    Motocicleta y tipo de motocicleta
    Se realizo la clase tipos de motocicleta y se realizaron modificaciones en la clase de motocicleta.

--- src/main/java/parcial2/Motocicleta.java
+++ src/main/java/parcial2/Motocicleta.java
@@ -1,23 +1,34 @@
 package parcial2;
 
 public class Motocicleta extends Vehiculo {
-    private TipoCombustible tipoMotocicleta;
-
-    public Motocicleta(String marca, String modelo, int año, TipoCombustible tipoMotocicleta) {
+    private TipoCombustible tipoCombustible;
+    private TipoMotocicleta tipoMotocicleta; 
+    
+    public Motocicleta(String marca, String modelo, int año, TipoCombustible tipoCombustible, TipoMotocicleta tipoMotocicleta) {
         super(marca, modelo, año);
-        this.tipoMotocicleta = tipoMotocicleta;
+        this.tipoCombustible = tipoCombustible;
+        this.tipoMotocicleta = tipoMotocicleta; 
     }
 
     @Override
     public String imprimirInformacion() {
-        return super.imprimirInformacion() + ", " + tipoMotocicleta;
+        return super.imprimirInformacion() + ",  combustión a " + tipoCombustible + ", tipo de motocicleta: " + tipoMotocicleta;
     }
 
-    public TipoCombustible getTipoMotocicleta() {
+    public TipoCombustible getTipoCombustible() {
+        return tipoCombustible;
+    }
+
+    public void setTipoCombustible(TipoCombustible tipoCombustible) {
+        this.tipoCombustible = tipoCombustible;
+    }
+
+   
+    public TipoMotocicleta getTipoMotocicleta() {
         return tipoMotocicleta;
     }
 
-    public void setTipoMotocicleta(TipoCombustible tipoMotocicleta) {
+    public void setTipoMotocicleta(TipoMotocicleta tipoMotocicleta) {
         this.tipoMotocicleta = tipoMotocicleta;
     }
 }
