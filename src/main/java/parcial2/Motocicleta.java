a57b6bb21963159c81e23dc30345a8ef4cebc4e6
invitado <invitado@gmail.com>
2024-06-06 08:22:30 -0500

    Motocicleta

--- src/main/java/parcial2/Motocicleta.java
+++ src/main/java/parcial2/Motocicleta.java
@@ -1,5 +1,23 @@
 package parcial2;
 
-public class Motocicleta {
-    // TODO: Tu código va aquí
+public class Motocicleta extends Vehiculo {
+    private TipoCombustible tipoMotocicleta;
+
+    public Motocicleta(String marca, String modelo, int año, TipoCombustible tipoMotocicleta) {
+        super(marca, modelo, año);
+        this.tipoMotocicleta = tipoMotocicleta;
+    }
+
+    @Override
+    public String imprimirInformacion() {
+        return super.imprimirInformacion() + ", " + tipoMotocicleta;
+    }
+
+    public TipoCombustible getTipoMotocicleta() {
+        return tipoMotocicleta;
+    }
+
+    public void setTipoMotocicleta(TipoCombustible tipoMotocicleta) {
+        this.tipoMotocicleta = tipoMotocicleta;
+    }
 }
